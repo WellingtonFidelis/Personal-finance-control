48c2a5ef3518d624c3e911c5d0c3ba24f046af28
WellingtonFidelis <wellingtonf20@gmail.com>
2020-09-15 22:56:57 -0300

    fetch to test api.get. -> pt-6

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import axios from "axios";
+
+const api = axios.create({ baseURL: 'api' });
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      // const axiosObject = await api.get('/transaction?period=2019-07');
+      const { data } = await api.get('/transaction?period=2019-02');
+      console.log(data);
+
+      setTransactions(data.transactions);
+    }
+
+    fetchTransactions();
+  }, []);
+  // array of dependecis not full will be once executed.
+  return (
+    <div className="container">
+      <h1 className="center">Final Challenge - Bootcamp Full Stack</h1>
+      {
+        transactions.map((transaction) => {
+          return <p key={transaction}>{transaction}</p>;
+        })
+      }
+    </div>
+  );
 }
