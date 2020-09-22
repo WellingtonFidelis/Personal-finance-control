abfe60e07e60c61b074b21aaa7373381bb6147bb
WellingtonFidelis <wellingtonf20@gmail.com>
2020-09-21 21:02:40 -0300

    methods post and put
    pt 11

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -29,4 +29,46 @@ transactionRouter.get('/', async (request, response) => {
   }
 });
 
+transactionRouter.post('/', async (request, response) => {
+  const { body } = request;
+
+  try {
+    if (JSON.stringify(body) === '{}') {
+      throw new Error(`Conteúdo inexistente`);
+    }
+
+    // Mongo DB
+
+    response.send({
+      status: 'Ok',
+    });
+  } catch ({ message }) {
+    console.log(message);
+    response.status(400).send({ error: message });
+  }
+});
+
+transactionRouter.put('/:id', async (request, response) => {
+  const { body, params } = request;
+
+  try {
+    // if (!params.id) {
+    //   throw new Error(`É necessário informar o Id da transação.`);
+    // }
+
+    if (JSON.stringify(body) === '{}') {
+      throw new Error(`Conteúdo inexistente`);
+    }
+
+    // Mongo DB
+
+    response.send({
+      status: 'Ok',
+    });
+  } catch ({ message }) {
+    console.log(message);
+    response.status(400).send({ error: message });
+  }
+});
+
 module.exports = transactionRouter;
