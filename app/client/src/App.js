7726e6fbe4942d0f7f7b14287a11ebf6a2a15556
WellingtonFidelis <wellingtonf20@gmail.com>
2020-10-19 22:10:05 -0300

    Trying to use react-bootstrap, I did not succeed.

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { Container, Form, Row, Col } from 'react-bootstrap';
 
 const api = axios.create({ baseURL: 'api' });
 
 export default function App() {
-
   const [transactions, setTransactions] = React.useState([]);
 
   React.useEffect(() => {
@@ -15,22 +14,22 @@ export default function App() {
       console.log(data.transactions);
 
       setTransactions(data.transactions);
-
-    }
+    };
 
     fetchTransactions();
   }, []);
-  // array of dependecis not full will be once executed.
+  // array of dependecies not full will be once executed.
+  /* trocar o bootstrap por html basic e usar styled Components */
   return (
     <Container>
-      <section style={{ backgroundColor: "yellow" }}>
+      <section style={{ backgroundColor: 'yellow' }}>
         <Row>
           <Col xs={12}>
             <h1 className="center">Final Challenge - Bootcamp Full Stack</h1>
           </Col>
         </Row>
       </section>
-      <section style={{ backgroundColor: "green" }}>
+      <section style={{ backgroundColor: 'green' }}>
         <Row>
           <Col></Col>
           <Col md={3} xs={12}>
@@ -38,34 +37,35 @@ export default function App() {
           </Col>
           <Col></Col>
         </Row>
-        <Row>
-          <Form>
-            <Row>
-              <Col className="fs-">
-                <Form.Group>
-                  <Form.Control as='select'>
-                    <option>OI</option>
-                  </Form.Control>
-                </Form.Group>
-
-              </Col>
-            </Row>
-          </Form>
-
-        </Row>
+        <Form>
+          <Row>
+            <Col></Col>
+            <Col xs={12} md={4}>
+              <Form.Group>
+                <Form.Control as="select">
+                  {transactions.map((transaction) => {
+                    return (
+                      <option key={transaction._id}>
+                        {transaction.yearMonth}
+                      </option>
+                    );
+                  })}
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Form>
       </section>
       <section>
         <Row>
           <Col>
-            {
-              transactions.map((transaction) => {
-                return <p key={transaction._id}>{transaction.description}</p>;
-              })
-            }
+            {transactions.map((transaction) => {
+              return <p key={transaction._id}>{transaction.description}</p>;
+            })}
           </Col>
         </Row>
       </section>
-
-    </Container >
+    </Container>
   );
 }
