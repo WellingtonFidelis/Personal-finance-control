3600376426efeac74546f749756741c49e1d4495
Wellington Fidelis <wellingtonf@pop-os.localdomain>
2020-10-25 20:10:51 -0300

    change tags and css

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { Container, Form, Row, Col } from 'react-bootstrap';
 
+import './index.css';
+
 const api = axios.create({ baseURL: 'api' });
 
 export default function App() {
@@ -21,51 +23,51 @@ export default function App() {
   // array of dependecies not full will be once executed.
   /* trocar o bootstrap por html basic e usar styled Components */
   return (
-    <Container>
-      <section style={{ backgroundColor: 'yellow' }}>
-        <Row>
-          <Col xs={12}>
+    <div className="container">
+      <section id="section-main-title">
+        <div className="row">
+          <div className="col col-xs-12">
             <h1 className="center">Final Challenge - Bootcamp Full Stack</h1>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </section>
-      <section style={{ backgroundColor: 'green' }}>
-        <Row>
-          <Col></Col>
-          <Col md={3} xs={12}>
+      <section id="section-financial">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col col-md-3 col-xs-12">
             <h3>Financial Control</h3>
-          </Col>
-          <Col></Col>
-        </Row>
-        <Form>
-          <Row>
-            <Col></Col>
-            <Col xs={12} md={4}>
-              <Form.Group>
-                <Form.Control as="select">
+          </div>
+          <div className="col"></div>
+        </div>
+        <form>
+          <div className="row">
+            <div className="col"></div>
+              <div className="col col-md-4 col-xs-12">
+              <div className="form-group">
+                <select className="form-control">
                   {transactions.map((transaction) => {
                     return (
-                      <option key={transaction._id}>
+                      <option className="option-text" key={transaction._id}>
                         {transaction.yearMonth}
                       </option>
                     );
                   })}
-                </Form.Control>
-              </Form.Group>
-            </Col>
-            <Col></Col>
-          </Row>
-        </Form>
+                </select>
+              </div>
+            </div>
+            <div className="col"></div>
+          </div>
+        </form>
       </section>
       <section>
-        <Row>
-          <Col>
+        <div className="row">
+          <div className="col">
             {transactions.map((transaction) => {
               return <p key={transaction._id}>{transaction.description}</p>;
             })}
-          </Col>
-        </Row>
+          </div>
+        </div>
       </section>
-    </Container>
+    </div>
   );
 }
